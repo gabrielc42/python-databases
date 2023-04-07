56ddaf0c6fc18cde40e975c0826bfdcd6195d570
gabrielc42 <clark.gabe97@gmail.com>
2023-04-07 11:51:06 -0500

    creating gui part 1, box opens and closes

---
+++ mybooks.py
@@ -0,0 +1,7 @@
+from tkinter import Tk, Button, Label, ScrollBar, ListBox, StringVar, Entry, W, E, N, S, END
+from tkinter import ttk
+from tkinter import messagebox
+
+root = Tk()
+
+root.mainloop()
