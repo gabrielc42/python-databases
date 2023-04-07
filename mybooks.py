5da6a255151b8ef65109b9e40a9285513dcb4ac8
gabrielc42 <clark.gabe97@gmail.com>
2023-04-07 11:59:16 -0500

    root settings, box still opens and closes error

--- mybooks.py
+++ mybooks.py
@@ -4,4 +4,9 @@ from tkinter import messagebox
 
 root = Tk()
 
+root.title("My Books Database Application")
+root.configure(background="light green")
+root.geometry("850x500")
+root.resizable(width=False,height=False)
+
 root.mainloop()
